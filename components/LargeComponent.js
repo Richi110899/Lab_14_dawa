b5afc47bdebba69ea13031fd056c26d6bd4364dd
Ricardo Antonio Gómez Sotomayor <ricardo.gomez@tecsup.edu.pe>
2025-06-24 21:43:22 -0500

    Creacion de la importacion en index.js

---
+++ components/LargeComponent.js
@@ -0,0 +1,8 @@
+export default function LargeComponent() {
+  return (
+    <div>
+      <h2>Componente Grande Dinámico</h2>
+      <p>Este es un componente cargado dinámicamente en Next.js</p>
+    </div>
+  );
+}
