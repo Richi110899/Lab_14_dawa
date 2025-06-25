aa2b7be8881174669b3df31834e1d10f2763ca08
Ricardo Antonio Gómez Sotomayor <ricardo.gomez@tecsup.edu.pe>
2025-06-24 21:39:26 -0500

    Modificacion en index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
+// Importa la función dynamic de Next.js para carga dinámica de componentes
 import dynamic from "next/dynamic";
 
+// Define un componente que se cargará dinámicamente sin renderizado en el servidor (ssr: false)
 const DynamicComponent = dynamic(() => import("../components/LargeComponent"), {
-  ssr: false,
+  ssr: false, // Desactiva renderizado en servidor para este componente
 });
 
+// Componente principal de la página Home
 export default function Home() {
   return (
     <>
+      {/* Título visible en la página */}
       <h1>Bienvenido</h1>
+
+      {/* Inserta el componente cargado dinámicamente */}
       <DynamicComponent />
     </>
   );
