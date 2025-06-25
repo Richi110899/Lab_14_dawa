fbd404c37c630c00b9e02fab233480b242d6fae9
Ricardo Antonio Gómez Sotomayor <ricardo.gomez@tecsup.edu.pe>
2025-06-24 21:09:44 -0500

    Doceavo commit

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,14 +1,13 @@
 const BASE_URL = "https://lab-14-dawa.vercel.app";
 
 export default async function handler(req, res) {
-  const urls = ["/", "/blog", "/contacto"];
+  const urls = ["/", "/blog", "/contacto"]; // Rutas públicas a indexar
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n
-    ${urls.map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`).join("")}\n
-  </urlset>`;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls.map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`).join("\n  ")}
+</urlset>`;
 
   res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+  res.status(200).send(sitemap); // Alternativa más estándar a write + end
 }
